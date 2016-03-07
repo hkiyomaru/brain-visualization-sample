4a82702a60beba218875475ec96c4b9461e5787d
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-03-07 17:14:05 +0900

    Tweaking

--- read_csv.py
+++ read_csv.py
@@ -2,20 +2,20 @@ import csv
 import numpy as np
 from parse_partition import ParsePartition
 
-link_data_path="datasets/nature13186-s4.csv"
-
 class Readcsv(ParsePartition):
-    def __init__(self, query, path, external):
+    def __init__(self, query, path, ldpath, external):
         ParsePartition.__init__(self, query, path)
         self.external = external
-        self.ldpath = link_data_path
+        self.ldpath = ldpath
         self.initialize()
 
     def initialize(self):
         reader = csv.reader(open(self.ldpath, 'r'))
         array = []
-        self.header = next(reader)[1:]
+        self.col_header = next(reader)[1:]
+        self.row_header = []
         for row in reader:
+            self.row_header.append(row[0])
             array.append(row[1:])
         array = np.array(array)
         self.conn_list = []
@@ -28,27 +28,27 @@ class Readcsv(ParsePartition):
     def set_external_conn(self, array):
         rn = 0
         for row in array:
-            if self.header[rn] not in self.targets:
+            if self.row_header[rn] not in self.targets:
                 pass
             else:
                 cn = 0
                 for tf in row:
-                    if tf != '0':
-                        self.conn_list.append((self.header[rn], self.header[cn]))
+                    if float(tf) > 0.1:
+                        self.conn_list.append((self.row_header[rn], self.col_header[cn]))
                     cn += 1
             rn += 1
 
     def set_internal_conn(self, array):
         rn = 0
         for row in array:
-            if self.header[rn] not in self.targets:
+            if self.row_header[rn] not in self.targets:
                 pass
             else:
                 cn = 0
                 for tf in row:
-                    if self.header[cn] in self.targets:
-                        if tf != '0':
-                            self.conn_list.append((self.header[rn], self.header[cn]))
+                    if self.col_header[cn] in self.targets:
+                        if float(tf) > 0.1:
+                            self.conn_list.append((self.row_header[rn], self.col_header[cn]))
                     cn += 1
             rn += 1
 
@@ -57,6 +57,6 @@ class Readcsv(ParsePartition):
         return list(set(self.conn_list))
 
 if __name__ == '__main__':
-    csv_reader = Readcsv("Hippocampal", "datasets/nature13186-s2.csv", False)
+    csv_reader = Readcsv("Hippocampal", "datasets/nature13186-s2.csv", "datasets/nature13186-s2.csv", False)
     print csv_reader.get_conn()
 
